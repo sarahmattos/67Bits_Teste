3d3d4a43fbf9a8ca3a740e520baaebceb41c1356
Sarah Mattos <sahmattos13@gmail.com>
2023-12-13 13:49:26 -0300

    capacidade, level e area de entrega

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerManager : MonoBehaviour
 {
    private Animator anim;// Referência do animator do player
    private PlayerTouchMovement playerTouchMovement;// Referência do script de touch
    private UnityEngine.AI.NavMeshAgent Player;// Referência do componente de navegação do player
+   GameObject target;
+   public int npcCount;
+   public int money;
+   public int level=1;
+   public int capacity;
+   [SerializeField] Renderer render;
+   [SerializeField] Material[] materials;
+   [SerializeField] TMP_Text countText;
+   [SerializeField] TMP_Text  moneyCount;
    void Start()
    {
         // Encontra os componentes
          anim = GetComponent<Animator>();
          playerTouchMovement = GetComponent<PlayerTouchMovement>();
          Player = GetComponent<UnityEngine.AI.NavMeshAgent>();
+         ConfigLevel(level);
+         countText.text = npcCount.ToString()+"/"+capacity;
    }
    private void Update()
     {
@@ -34,14 +47,73 @@ public class PlayerManager : MonoBehaviour
         
         // Atualiza a animação com base no movimento
         anim.SetBool("Moving", scaledMovement != Vector3.zero);
-        if( scaledMovement != Vector3.zero){
-            anim.ResetTrigger("Stabling"); 
-            anim.SetTrigger("StopStable");
-        }
         
     }
     public void Stable(){
          anim.SetTrigger("Stabling");
-          anim.ResetTrigger("StopStable");
     }
+    public void GainMoney(){
+        if(npcCount>0){
+            money += npcCount*10;
+            npcCount= 0;
+            countText.text = npcCount.ToString()+"/"+capacity;
+            moneyCount.text = money.ToString();
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        target = other.gameObject;
+        if(target.tag =="NPC")
+        {
+            if(npcCount<capacity){
+                Stable();
+                Invoke("DestroyNPC", 0.7f);
+            }
+            
+        }
+        if(target.tag =="MoneyArea")
+        {
+            GainMoney();
+            //Invoke("DestroyNPC", 0.7f);
+        }
+    }
+    void DestroyNPC()
+    {
+        npcCount++;
+        countText.text = npcCount.ToString()+"/"+capacity;
+        Destroy(target);
+    }
+
+    void ConfigLevel(int level)
+    {
+        // Use um bloco switch para configurar com base no nível
+        switch (level)
+        {
+            case 1:
+                capacity = 2;
+                render.material= materials[0];
+                break;
+
+            case 2:
+                capacity = 3;
+                render.material= materials[1];
+                break;
+
+            case 3:
+                capacity = 4;
+                render.material= materials[2];
+                break;
+
+            default:
+                Debug.LogWarning("Nível desconhecido. Não foi possível configurar.");
+                break;
+        }
+    }
+    public void SetLevel(){
+        level++;
+        ConfigLevel(level);
+        countText.text = npcCount.ToString()+"/"+capacity;
+    }
+    
 }
